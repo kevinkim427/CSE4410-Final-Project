e355b09fb6de5dfdfaa76b42ceed04f9362de320
kevinkim427 <kevinkim427@gmail.com>
2024-04-13 10:13:09 -0700

    commit

---
+++ Final Project Dark Souls/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DS
+{
+    public class Item : ScriptableObject
+    {
+        [Header("Item Information")]
+        public Sprite itemicon;
+        public string itemname;
+
+    }
+}
---
+++ Final Project Dark Souls/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DS
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        InputHandler inputHandler;
+        Animator anim;
+        CameraHandler cameraHandler;
+        PlayerLocomotion playerLocomotion;
+
+        public bool isInteracting;
+        [Header("Player Flags")]  
+        public bool isSprinting;
+
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponentInChildren<Animator>();
+            playerLocomotion = GetComponent<PlayerLocomotion>();
+            cameraHandler = CameraHandler.singleton;
+        }
+
+        void Update()
+        {
+            float delta = Time.deltaTime;
+            isInteracting = anim.GetBool("isInteracting");
+            
+            inputHandler.TickInput(delta);
+            playerLocomotion.HandleMovement(delta);
+            playerLocomotion.HandleRollingAndSprinting(delta);                           
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+            if(cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, inputHandler.mouseX, inputHandler.mouseY);
+            }
+        }
+
+        private void LateUpdate()
+        {            
+            inputHandler.rollFlag = false;
+            inputHandler.sprintFlag = false;
+        }
+    }
+}
